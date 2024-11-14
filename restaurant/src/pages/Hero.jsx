8287122243f729dea2aa53b857575883074d6a80
Md Ahasan Habib Nahid <ahasanhnahid@gmail.com>
2024-11-14 12:08:44 +0600

    fix bug fix hero screen size

--- restaurant/src/pages/Hero.jsx
+++ restaurant/src/pages/Hero.jsx
@@ -7,17 +7,17 @@ import vector from '../images/Vector.png'
 
 function Hero() {
   return (
-    <section className=" max-w-[1373px] min-h-[790px] md:min-h-[750px]  xl:min-h-screen flex flex-col justify-between pt-32 lg:justify-normal px-6  pb-6 lg:pb-0 gap-6 mx-auto   md:relative  lg:flex-row  text-white ">
+    <section className=" max-w-[1373px] min-h-[790px] md:min-h-[750px]  xl:min-h-[850px]  2xl:min-h-[1000px] flex flex-col justify-between pt-28 lg:justify-normal px-6  pb-6 lg:pb-0 gap-6 mx-auto   md:relative  lg:flex-row  text-white ">
 
       <article className='flex flex-col gap-8 md:justify-center z-20  '>
         <h1 className="font-heading text-5xl md:text-6xl lg:text-8xl xl:text-9xl md:pr-10 lg:py-5 lg:bg-gradient-to-r from-transparent to-red-800 ">
-          Taste the Authentic <br className='hidden md:block'/> Saudi Cuisine
+          Taste the Authentic <br className='hidden lg:block'/> Saudi Cuisine
         </h1>
         <p className='font-sans text-xl md:w-[500px] lg:w-[450px] xl:w-[559px] md:text-2xl md:text-[24px] '>Among the best Saudi chefs in the world, serving you something beyond flavor. </p>
         <Button label="Explore Menu" className='bg-yellow-400 hover:bg-yellow-500 text-black' />
       </article>
 
-      <aside className='relative lg:absolute right-0 lg:right-10 2xl:right-0 md:top-1/2 lg:-translate-y-1/2  lg:w-[45%] xl:w-[40%] 2xl:w-auto '>
+      <aside className='relative lg:absolute right-0 lg:right-10  2xl:right-0 md:top-1/2 lg:-translate-y-1/2  lg:w-[45%] 2xl:w-auto '>
         <img src={heroImage} alt="hero image" className="relative z-10 w-full" />
         <img src={offerIcon}  className='w-24 absolute bottom-0 right-0 -translate-y-0 z-20 lg:w-32 xl:translate-x-1/2 xl:translate-y-4'/>
         <img src={vector} className="absolute top-0 right-0 -translate-y-1/2 z-0 xl:z-20 xl:translate-x-1/2 xl:-top-2" />
